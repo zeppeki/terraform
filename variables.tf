0d5601de554af78a009cf9f3ebc5e0577ad4bf68
root <root@ip-172-31-41-146.ap-northeast-1.compute.internal>
2019-02-03 12:18:15 +0000

    modify

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "ap-northeast-1"
+}
+
+variable "name" {
+  default = "tf"
+}
+
--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,39 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  
+  tags {
+    Name = "${var.name}-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"  
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags { 
+    Name = "${var.name}-igw"
+  }
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id = "${aws_vpc.vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0"
-  gateway_id = "${aws_internet_gateway.igw.id}"
+resource "aws_route_table" "public_rt" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
 }
 
-resource "aws_subnet" "subnet" {
-  vpc_id = "{$aws_vpc.vpc.id}"
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
+
+  tags {
+    Name = "${var.name}-public"
+  }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+
